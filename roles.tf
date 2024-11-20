e8477b15cf5f751ef9c43c18111b0fc3656b3013
aditya <adityanavaneethan98@gmail.com>
2024-11-20 10:55:29 +0000

    added more permissions to IAM role

--- roles.tf
+++ roles.tf
@@ -65,7 +65,3 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy_attachm
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_s3_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
