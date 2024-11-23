62507bd970f00ea002d63d7e4364758e25f67dfa
aditya <adityanavaneethan98@gmail.com>
2024-11-23 09:37:23 +0000

    new

--- roles.tf
+++ roles.tf
@@ -20,9 +20,9 @@ resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSLoadBalancingPolicy" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
@@ -30,9 +30,9 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSLoadBalancingPolicy" {
+resource "aws_iam_role_policy_attachment" "elastic_load_balancing_full_access" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
